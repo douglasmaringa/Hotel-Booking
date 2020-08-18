53dadeb62abca36fa7c46fa208ee74bf703f58f3
Douglas Maringa <douglasmaringa4@gmail.com>
2020-08-18 12:52:33 +0200

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,11 +6,10 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+
 import Typography from '@material-ui/core/Typography';
 function Checkout({title,id,image,price,about}) {
-    const [{ basket }, dispatch] = useStateValue();
+    const [ dispatch] = useStateValue();
     const removeFromBasket = () => {
         // remove item from basket...
         dispatch({
